24e1ae64e32b0282296fcd69f0359323203936ce
khalidMujahid <binkhalid267@gmail.com>
2022-08-28 15:06:19 +0100

    Added

--- routes/login.route.js
+++ routes/login.route.js
@@ -5,7 +5,6 @@ const router = require("express").Router();
 const bcrypt = require("bcrypt");
 
 router.post("/auth/login", async (req, res) => {
-  console.log(req.body);
   try {
     const { username, password } = req.body;
 
